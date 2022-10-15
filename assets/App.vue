5fcae34a6a339a98e80d2fa71940db67a7c0b6cf
Tu Nombre <you@example.com>
2022-10-15 13:19:50 +0200

    initial config

--- assets/App.vue
+++ assets/App.vue
@@ -1,5 +1,5 @@
 <template>
-    <h1>Layout</h1>
+    <h1>¡Hola Vue!</h1>
 </template>
 
 <script>
--- assets/views/Home.vue
+++ assets/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        hola
+        hola asdfasdfasd
     </div>
 </template>
 
